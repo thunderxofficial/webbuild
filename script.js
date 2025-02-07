cc63b1a3b77dd234db5e3865430f00e18ad37ab9
boltxyzcode <potatogamer34579@gmail.com>
2025-02-07 19:26:21 +0100

    adblock test updated

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const button1 = document.getElementById("button 1");
 
-var e = document.createElement('div');
-e.id = 'CyVkbnSaiUrJ';
-e.style.display = 'none';
-document.body.appendChild(e);
-
-if(document.getElementById('CyVkbnSaiUrJ')) {
-console.log('Ad block test passed.');
-} else {
-alert('It seems that you are using an ad blocker. Please disable it to help us analyze our website traffic.');
-window.location.href = 'https://www.wikihow.com/Disable-AdBlock';
-}
+(function() {
+    var _0x1a3b = document.createElement('script');
+    _0x1a3b.src = 'https://doubleclick.com';
+    _0x1a3b.async = true;
+  
+    _0x1a3b.onload = function() {
+      console['log']('Ad block test passed: DoubleClick script loaded successfully.');
+    };
+  
+    _0x1a3b.onerror = function() {
+      alert('It seems that you are using an ad blocker. Please disable it to help us analyze our website traffic.');
+      window['location']['href'] = 'https://www.wikihow.com/Disable-AdBlock';
+    };
+  
+    document['head']['appendChild'](_0x1a3b);
+  })();
+  
 
 if (button1 && localStorage.setupCompleted) {
     button1.innerText = "Panel";
